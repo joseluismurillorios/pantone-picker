582ec3e7194fcd57f7104d92fdc0ac257ff9168d
Jose Murillo <joseluismurillorios@gmail.com>
2020-06-25 16:11:04 -0700

    Fixed script action

--- scripts/gh-page-deploy.sh
+++ scripts/gh-page-deploy.sh
@@ -1,10 +1,10 @@
-# git checkout --orphan gh-pages 
-# npm run build
+git checkout --orphan gh-pages 
+npm run build
 
-# # (for react: replace dist with build)
-# git --work-tree dist add --all
-# git --work-tree dist commit -m 'Deploy'
-# git push origin HEAD:gh-pages --force
-# rm -r dist
-# git checkout -f master
-# git branch -D gh-pages
+# (for react: replace dist with build)
+git --work-tree dist add --all
+git --work-tree dist commit -m 'Deploy'
+git push origin HEAD:gh-pages --force
+rm -r dist
+git checkout -f master
+git branch -D gh-pages
