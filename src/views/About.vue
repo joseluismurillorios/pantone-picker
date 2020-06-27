652dc5d61151aef593faeebf33d72986a9779e97
Jose Murillo <joseluismurillorios@gmail.com>
2020-06-27 15:30:11 -0700

    Added color book

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="about p-4">
-    <h1>This is an about page</h1>
+    <div class="p-4 w-full bg-white rounded shadow-sm">
+      <h1>This is an about page</h1>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'SearchBar',
+  data: () => ({
+    book: {},
+  }),
+  created() {
+    fetch('/books/PANTONE SkinTone™ Guide.json').then((response) => response.json())
+      .then((book) => {
+        console.log(book);
+        this.book = book;
+      });
+  },
+};
+</script>
