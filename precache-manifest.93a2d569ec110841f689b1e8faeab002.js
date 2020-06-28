6c7b08e05bfb52ed3af99a8152bc4fda86c30617
Jose Murillo <joseluismurillorios@gmail.com>
2020-06-28 08:18:53 +0000

    gh-pages

---
+++ precache-manifest.93a2d569ec110841f689b1e8faeab002.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78505dea1a138bc088395ca47a5b35b8",
+    "url": "/pantone-picker/books/ANPA Color.json"
+  },
+  {
+    "revision": "db751dd75ca6de179aaff6dd05059564",
+    "url": "/pantone-picker/books/DIC Color Guide.json"
+  },
+  {
+    "revision": "a1952c1560b28a88ab4874b9b6811f30",
+    "url": "/pantone-picker/books/FOCOLTONE.json"
+  },
+  {
+    "revision": "1b3e59298a251388e8bd4ad512d7fec6",
+    "url": "/pantone-picker/books/HKS E Process.json"
+  },
+  {
+    "revision": "2d6adff026f4623d4e6a9774a7501a32",
+    "url": "/pantone-picker/books/HKS E.json"
+  },
+  {
+    "revision": "b944eb1ec73632c7054f9650e3357f7d",
+    "url": "/pantone-picker/books/HKS K Process.json"
+  },
+  {
+    "revision": "274e152053a35874c120f34f6427b634",
+    "url": "/pantone-picker/books/HKS K.json"
+  },
+  {
+    "revision": "ba2e91af1623a80207c7e0e6424bc6fe",
+    "url": "/pantone-picker/books/HKS N Process.json"
+  },
+  {
+    "revision": "65c71f1e07e0dd0e90552127ad1c0876",
+    "url": "/pantone-picker/books/HKS N.json"
+  },
+  {
+    "revision": "41acabe8391b0eb57ba1d5b1c4a4173c",
+    "url": "/pantone-picker/books/HKS Z Process.json"
+  },
+  {
+    "revision": "dc42e5edba75186dd759e34150c9eaf1",
+    "url": "/pantone-picker/books/HKS Z.json"
+  },
+  {
+    "revision": "46ee9906a10c8e277d4f8a0728981d3f",
+    "url": "/pantone-picker/books/PANTONE SkinTone™ Guide.json"
+  },
+  {
+    "revision": "80d79f5427a557857669598ba9c3845b",
+    "url": "/pantone-picker/books/PANTONE+ CMYK Coated.json"
+  },
+  {
+    "revision": "7329926649f24301e43cd8bee2375b75",
+    "url": "/pantone-picker/books/PANTONE+ CMYK Uncoated.json"
+  },
+  {
+    "revision": "a5a072117f8cfe07e0ddf6e215488a80",
+    "url": "/pantone-picker/books/PANTONE+ Color Bridge Coated-V3.json"
+  },
+  {
+    "revision": "62e25a5b2f3895f3758c1662f56afe95",
+    "url": "/pantone-picker/books/PANTONE+ Color Bridge Coated.json"
+  },
+  {
+    "revision": "e04f60065d6263dc2355a726a6059636",
+    "url": "/pantone-picker/books/PANTONE+ Color Bridge Uncoated-V3.json"
+  },
+  {
+    "revision": "9a38bbd5aa7f35368dd6415f77a75773",
+    "url": "/pantone-picker/books/PANTONE+ Color Bridge Uncoated.json"
+  },
+  {
+    "revision": "3ae9d0e10d3ecdbfbef7506eba2dcbf8",
+    "url": "/pantone-picker/books/PANTONE+ Extended Gamut Coated.json"
+  },
+  {
+    "revision": "1c3f28afbfbd5ab932f5b39248babb95",
+    "url": "/pantone-picker/books/PANTONE+ Metallic Coated.json"
+  },
+  {
+    "revision": "d5942e69fa2e8d9298f448de3f92431c",
+    "url": "/pantone-picker/books/PANTONE+ Metallics Coated.json"
+  },
+  {
+    "revision": "af916a88c39be9cac7344261519c46a9",
+    "url": "/pantone-picker/books/PANTONE+ Pastels & Neons Coated.json"
+  },
+  {
+    "revision": "d1bd6793f203305b295cfaa8494a3b5f",
+    "url": "/pantone-picker/books/PANTONE+ Pastels & Neons Uncoated.json"
+  },
+  {
+    "revision": "f0b694df45e07fc46471e002669338da",
+    "url": "/pantone-picker/books/PANTONE+ Premium Metallics Coated.json"
+  },
+  {
+    "revision": "d69dc4c477a04c42f231fc541933a402",
+    "url": "/pantone-picker/books/PANTONE+ Solid Coated-V3.json"
+  },
+  {
+    "revision": "7c5439016342602c36dc6f49768bf835",
+    "url": "/pantone-picker/books/PANTONE+ Solid Coated.json"
+  },
+  {
+    "revision": "d28d20d67b2ca08bc58de4752f765d31",
+    "url": "/pantone-picker/books/PANTONE+ Solid Uncoated-V3.json"
+  },
+  {
+    "revision": "35994d5ce9df3b61774d6be48d230204",
+    "url": "/pantone-picker/books/PANTONE+ Solid Uncoated.json"
+  },
+  {
+    "revision": "ca99dd9a66fcc5936211c1b7f91eccb0",
+    "url": "/pantone-picker/books/PANTONE® F+H cotton TCX.json"
+  },
+  {
+    "revision": "c176851cbe74063d576011aee7a34a07",
+    "url": "/pantone-picker/books/PANTONE® F+H nylon brights TN.json"
+  },
+  {
+    "revision": "0e699d9c567df3ae727c11fdfa6ab8bf",
+    "url": "/pantone-picker/books/PANTONE® FHI Color Guide.json"
+  },
+  {
+    "revision": "2a5641872109fc1f434f3ec2258805e7",
+    "url": "/pantone-picker/books/PANTONE® GoeBridge™ coated.json"
+  },
+  {
+    "revision": "771b1c963b1168def0f1e4cf900f1710",
+    "url": "/pantone-picker/books/PANTONE® Goe™ coated.json"
+  },
+  {
+    "revision": "2507a13a9921e0503d9ec44c5a2caf61",
+    "url": "/pantone-picker/books/PANTONE® Goe™ uncoated.json"
+  },
+  {
+    "revision": "8b641e3f7574de2259a169b2b011f015",
+    "url": "/pantone-picker/books/TOYO 94 COLOR FINDER.json"
+  },
+  {
+    "revision": "93cf3b5d31d47bdf8a79ddc0efd4ad3c",
+    "url": "/pantone-picker/books/TOYO COLOR FINDER.json"
+  },
+  {
+    "revision": "71608e9982bd2122ad04afa268769446",
+    "url": "/pantone-picker/books/TRUMATCH.json"
+  },
+  {
+    "revision": "352d631969e077a0c621",
+    "url": "/pantone-picker/css/app.4569b040.css"
+  },
+  {
+    "revision": "c3f3c163839291d0a976",
+    "url": "/pantone-picker/css/books.d3260342.css"
+  },
+  {
+    "revision": "c7616111959845015ac4a3ba1a188dd5",
+    "url": "/pantone-picker/index.html"
+  },
+  {
+    "revision": "2e7ed773bfb0f24a2fd2",
+    "url": "/pantone-picker/js/about.9faed428.js"
+  },
+  {
+    "revision": "352d631969e077a0c621",
+    "url": "/pantone-picker/js/app.a19c19bc.js"
+  },
+  {
+    "revision": "c3f3c163839291d0a976",
+    "url": "/pantone-picker/js/books.6abfd5c5.js"
+  },
+  {
+    "revision": "56d6f13d10f3cb2bb79b",
+    "url": "/pantone-picker/js/chunk-vendors.500c7f2e.js"
+  },
+  {
+    "revision": "f2ad669120cee079b06dbfc0ccc076af",
+    "url": "/pantone-picker/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pantone-picker/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/pantone-picker/precache-manifest.93a2d569ec110841f689b1e8faeab002.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "pantone-picker"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
